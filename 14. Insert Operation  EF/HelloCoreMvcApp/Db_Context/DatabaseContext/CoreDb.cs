aee59e15750c01a007f71a059925c55654455802
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2020-01-15 03:30:48 +0600

    Insert Operation
    1. Install package in Db_Context library
    -Microsoft.EntityFreamworkCore
    -Microsoft.EntityFreamworkCore.Sql
    -Microsoft.EntityFreamworkCore.Design
    -Microsoft.EntityFreamworkCore.Tools
    
    2. Add migration Db_Context library
    3. Write insert operation code in CustomerController.

---
+++ 14. Insert Operation  EF/HelloCoreMvcApp/Db_Context/DatabaseContext/CoreDb.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Db_Context.DatabaseContext
+{
+    public class CoreDb : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = @"Data Source = DESKTOP-HHA0TOP; 
+                                    Initial Catalog = CustomerDb; 
+                                    Integrated Security = true";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
