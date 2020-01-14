999ca327ed02b1638f2b3cf352d81a06a58f202e
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2020-01-14 20:31:55 +0600

    Segregating Projects
    1. Add Models class library, move all of the model in this library model.
    2. Add CoreDbContext library, move migration file and DatabaseContext file.
    3. Add references.

---
+++ 14. Segregating Projects/HelloCoreMvcApp/CoreDbContext/DatabaseContext/CoreDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDbContext.DatabaseContext
+{
+    public class CoreDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = @"Data Source = DESKTOP-HHA0TOP; 
+                                        Initial Catalog = CustomerDb;
+                                        Integrated Security = true";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
