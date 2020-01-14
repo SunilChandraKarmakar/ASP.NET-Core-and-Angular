5e9abef2bfaf1f931339303b8e62fecc1fa9780d
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2020-01-14 17:57:32 +0600

    Insert Operation Using Entiy Framework
    1. Create new folder DatabaseContext.
    2. Create CoreContextDb class and Inherit ContextDb class. Create auto properity Customers.
    3. Override OnConfiguring method for ContextDb class, write code connection string.
    4. Add-Migration Initial and Update-Database. And aslo update Customer model.
    5. Add validation

---
+++ 13. Insert Operation EF/HelloCoreMvcApp/HelloCoreMvcApp/DatabaseContext/CoreDbContext.cs
@@ -0,0 +1,22 @@
+﻿using HelloCoreMvcApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloCoreMvcApp.DatabaseContext
+{
+    public class CoreDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = @"Data Source = DESKTOP-HHA0TOP; 
+                                        Initial Catalog = CustomerDb;
+                                        Integrated Security = true";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
