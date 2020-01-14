f2d3ac228dc3609ebfdab3026c7d2f5b63a0a4e7
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2020-01-14 22:45:39 +0600

    Segregation Project
    1. Add Models class library. There we will write all of the model of this project.
    2. Add DbContext class library. In this part, we will add my database context class and all migration file ar add.

---
+++ 13. Segregation Project/HelloCoreMvcApp/HelloCoreMvcApp/Controllers/CustomerController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Models;
+
+namespace HelloCoreMvcApp.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Create()
+        {
+            ViewBag.AddressList = AddressList();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Customer aCustomer)
+        {
+            if (ModelState.IsValid)
+            {
+                //Database code hare
+                return RedirectToAction("Details", aCustomer);
+            }                      
+            else
+            {
+                ViewBag.AddressList = AddressList();
+                return View(aCustomer);
+            }
+                
+        }
+
+        public IActionResult Details(Customer aCustomer)
+        {
+            ViewBag.Customer = aCustomer;
+            return View();
+        }
+
+        public List<SelectListItem> AddressList()
+        {
+            List<SelectListItem> address = new List<SelectListItem>()
+            {
+                new SelectListItem { Value = "DHK", Text = "Dhaka"},
+                new SelectListItem { Value = "CTG", Text = "Chittagong"},
+                new SelectListItem { Value = "Lax", Text = "Lakshmipur"},
+            };
+
+            return address;
+        }
+    }
+}
---
+++ 13. Segregation Project/HelloCoreMvcApp/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 5)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+    }
+}
