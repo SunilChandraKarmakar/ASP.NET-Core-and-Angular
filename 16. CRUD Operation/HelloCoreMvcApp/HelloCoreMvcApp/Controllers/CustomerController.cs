5abc243c707bb46313dfe16506ce4fa877005e83
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2020-01-16 02:00:24 +0600

    CRUD Operation
    1. Working with validation for CustomerController Create function View.
    2. Create ProjectRepositorys class library.
    3. Create CustomerRepositorys class.
    4. Create GetAllCustomer method for CustomerRepository class.
    5. Create Index function, view.
    6. Working with create method CustomerRepository class and update CustomerController Create function code.
    7. Create Edit function on CustomerRepository class and Controller

---
+++ 16. CRUD Operation/HelloCoreMvcApp/HelloCoreMvcApp/Controllers/CustomerController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Db_Context.DatabaseContext;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Models;
+using ProjectRepositorys;
+
+namespace HelloCoreMvcApp.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private CustomerRepository _customerRepository = new CustomerRepository();
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(_customerRepository.GetAllCustomer());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.AddressList = AddressList();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Customer aCustomer)
+        {
+            if (ModelState.IsValid)
+            {
+                bool isSaved = _customerRepository.Create(aCustomer);
+
+                if(isSaved)
+                    return RedirectToAction("Index", "Customer");
+                else
+                    return ViewBag.ErrorMessage = "Customer not saved!";
+            }                      
+            else
+            {
+                ViewBag.AddressList = AddressList();
+                return View(aCustomer);
+            }                     
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? Id)
+        {
+            if (Id == null)
+                return BadRequest();
+
+            Customer aCustomer = _customerRepository.FindCustomer(Id);
+
+            if (aCustomer == null)
+                return NotFound();
+
+            return View(aCustomer);
+
+        }
+
+        public IActionResult Details(Customer aCustomer)
+        {
+            ViewBag.Customer = aCustomer;
+            return View();
+        }
+
+        public List<SelectListItem> AddressList()
+        {
+            List<SelectListItem> address = new List<SelectListItem>()
+            {
+                new SelectListItem { Value = "DHK", Text = "Dhaka"},
+                new SelectListItem { Value = "CTG", Text = "Chittagong"},
+                new SelectListItem { Value = "Lax", Text = "Lakshmipur"},
+            };
+
+            return address;
+        }
+    }
+}
---
+++ 16. CRUD Operation/HelloCoreMvcApp/ProjectRepositorys/CustomerRepository.cs
@@ -0,0 +1,35 @@
+﻿using Db_Context.DatabaseContext;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectRepositorys
+{
+    public class CustomerRepository
+    {
+        private CoreDb _db = new CoreDb(); 
+
+        public List<Customer> GetAllCustomer()
+        {
+            return _db.Customers.ToList(); 
+        }
+
+        public bool Create(Customer aCustomer)
+        {
+            _db.Customers.Add(aCustomer);
+            return _db.SaveChanges() > 0;
+        }
+
+        public Customer FindCustomer(int Id)
+        {
+            return _db.Customers.Find(Id);
+        }
+
+        public Customer FindCustomer(int? id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
