814471c6e2de9a2e3587bf4838ba0cfc656b3c23
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2020-01-27 21:17:09 +0600

    ObjectModeling RelationshipEstablish
    1. Create model with relation Category, Product, Shop, Order, OrderDetails in ModelProject

---
+++ 17. ObjectModeling RelationshipEstablish/HelloCoreMvcApp/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public Customer Customer { get; set; }
+        public string OrderNo { get; set; }
+
+        public IEnumerable<OrderDetails> OrderDetails { get; set; }
+    }
+}
---
+++ 17. ObjectModeling RelationshipEstablish/HelloCoreMvcApp/ProjectRepositorys/CustomerRepository.cs
@@ -0,0 +1,43 @@
+﻿using Db_Context.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectRepositorys
+{
+    public class CustomerRepository
+    {
+        private CoreDb _db = new CoreDb(); 
+
+        public List<Customer> GetAllCustomer()
+        {
+            return _db.Customers.ToList(); 
+        }
+
+        public bool Create(Customer aCustomer)
+        {
+            _db.Customers.Add(aCustomer);
+            return _db.SaveChanges() > 0;
+        }
+
+        public Customer FindCustomer(int? Id)
+        {
+            return _db.Customers.Find(Id);
+        }
+
+        public bool Edit(Customer aCustomer)
+        {
+            _db.Entry(aCustomer).State = EntityState.Modified;
+            return _db.SaveChanges() > 0;
+        }
+
+        public bool Delete(Customer aCustomer)
+        {
+            _db.Customers.Remove(aCustomer);
+            return _db.SaveChanges() > 0;
+        }
+    }
+}
