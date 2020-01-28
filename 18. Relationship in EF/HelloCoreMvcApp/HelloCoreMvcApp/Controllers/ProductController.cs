0b345679a2391bdf3af90741b97b6a87746ff91d
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2020-01-28 23:40:59 +0600

    Relationship in EF
    1. Create ProductController and ProductRepositories
    2. Working with Index function and view, Create function and view in ProductController and ProductRepository

---
+++ 18. Relationship in EF/HelloCoreMvcApp/HelloCoreMvcApp/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProjectRepositorys;
+
+namespace HelloCoreMvcApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ProductRepository _productRepository = new ProductRepository();
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(_productRepository.GetAllProducts());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.CategoryList = _productRepository.GetAllCategory();
+            return View();
+        }
+    }
+}
---
+++ 18. Relationship in EF/HelloCoreMvcApp/ProjectRepositorys/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Db_Context.DatabaseContext;
+using Models;
+
+namespace ProjectRepositorys
+{
+    public class ProductRepository
+    {
+        private CoreDb _coreDb = new CoreDb();
+        
+        public List<Product> GetAllProducts()
+        {
+            return _coreDb.Products.ToList();
+        }
+
+        public List<Category> GetAllCategory()
+        {
+            return _coreDb.Categories.ToList();
+        }
+    }
+}
