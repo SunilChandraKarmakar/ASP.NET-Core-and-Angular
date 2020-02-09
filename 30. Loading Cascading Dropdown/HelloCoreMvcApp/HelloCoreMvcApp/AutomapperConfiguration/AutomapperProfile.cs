754a0e56bdd14870dae32704d431ee03a18b8e93
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2020-02-09 22:52:17 +0600

    Loading Cascading Dropdown
    1. Create CategoryWiseProductViewModel class.
    2. Create CategoryWiseProduct function and view in ProductController.
    3. Create GetProductByCategoryId Json function in ProductController
    4. Using JavaScrips and Ajax in CategoryWiseProduct view for dropdown cascading

---
+++ 30. Loading Cascading Dropdown/HelloCoreMvcApp/HelloCoreMvcApp/AutomapperConfiguration/AutomapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using HelloCoreMvcApp.Models.Product;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloCoreMvcApp.AutomapperConfiguration
+{
+    public class AutomapperProfile : Profile
+    {
+        public AutomapperProfile()
+        {
+            CreateMap<Product, ProductIndexViewModel>();
+            CreateMap<ProductCreateViewModel, Product>();
+        }
+    }
+}
---
+++ 30. Loading Cascading Dropdown/HelloCoreMvcApp/HelloCoreMvcApp/Controllers/CityController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business_Logic_Layer;
+using Business_Logic_Layer.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Models;
+using ProjectRepositorys;
+using ProjectRepositorys.Contracts;
+
+namespace HelloCoreMvcApp.Controllers
+{
+    public class CityController : Controller
+    {
+        private readonly ICityManager _iCityManager;
+        private readonly ICountryManager _iCountryManager;
+
+        public CityController(ICityManager iCityManager, ICountryManager iCountryManager)
+        {
+            _iCityManager = iCityManager;
+            _iCountryManager = iCountryManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(_iCityManager.GetAll());
+        }
+
+        private List<SelectListItem> CountryListItem()
+        {
+            List<SelectListItem> countryListItem = _iCountryManager.GetAll()
+                                                    .Select(c => new SelectListItem()
+                                                    {
+                                                        Value = c.Id.ToString(),
+                                                        Text = c.Name
+                                                    })
+                                                    .ToList();
+            return countryListItem;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {        
+            ViewBag.CountryListItem = CountryListItem();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(City aCity)
+        {
+            if(ModelState.IsValid)
+            {
+                bool isSave = _iCityManager.Add(aCity);
+
+                if (isSave)
+                    return RedirectToAction("Index", "City");
+                else
+                    return ViewBag.ErrorMessage = "City failed to save!";
+            }
+
+            ViewBag.CountryListItem = CountryListItem();
+            return View(aCity);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? Id)
+        {
+            if (Id == null)
+                return NotFound();
+
+            City aCity = _iCityManager.GetById(Id);
+
+            if (aCity == null)
+                return NotFound();
+
+            ViewBag.CountryListItem = CountryListItem();
+            return View(aCity);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(City aCity)
+        {
+            if(ModelState.IsValid)
+            {
+                bool isUpdate = _iCityManager.Update(aCity);
+
+                if (isUpdate)
+                    return RedirectToAction("Index", "City");
+                else
+                    return ViewBag.ErrorMessage = "City update failed!";
+            }
+
+            ViewBag.CountryListItem = CountryListItem();
+            return View(aCity);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? Id)
+        {
+            if (Id == null)
+                return NotFound();
+
+            City aCity = _iCityManager.GetById(Id);
+
+            if (aCity == null)
+                return NotFound();
+
+            return View(aCity);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(City aCity)
+        {
+            bool isDelete = _iCityManager.Remove(aCity);
+
+            if (isDelete)
+                return RedirectToAction("Index", "City");
+            else
+                return ViewBag.Error = "City delete is failed!";
+        }
+    }
+}
