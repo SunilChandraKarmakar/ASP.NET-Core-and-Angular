c95dfafb0e983fa081d0543364f89f13ec462a48
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2020-02-13 11:32:03 +0600

    Web API
    Working with Post, Put and Delete function in product api controller

--- 33. Web API/HelloCoreMvcApp/HelloCoreMvcApp/Controllers/APIController/ProductController.cs
+++ 33. Web API/HelloCoreMvcApp/HelloCoreMvcApp/Controllers/APIController/ProductController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Business_Logic_Layer.Contracts;
+using HelloCoreMvcApp.Models.Product;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Models;
@@ -14,10 +16,12 @@ namespace HelloCoreMvcApp.Controllers.APIController
     public class ProductController : ControllerBase
     {
         private readonly IProductManager _iProductManager;
+        private readonly IMapper _iMapper;
 
-        public ProductController(IProductManager iProductManager)
+        public ProductController(IProductManager iProductManager, IMapper iMapper)
         {
             _iProductManager = iProductManager;
+            _iMapper = iMapper;
         }
 
         [HttpGet]
@@ -30,6 +34,62 @@ namespace HelloCoreMvcApp.Controllers.APIController
         public Product Get(int id)
         {
             return _iProductManager.GetById(id);
+        }
+
+        [HttpPost]
+        public IActionResult Post(ProductCreateViewModel productCreateViewModel)
+        {
+            if(ModelState.IsValid)
+            {
+                Product aProduct = _iMapper.Map<Product>(productCreateViewModel);
+
+                bool isSave = _iProductManager.Add(aProduct);
+
+                if (isSave)
+                    return Ok(aProduct);
+                else
+                    return BadRequest(new { ErrorMessage = "Product post is failed!" });
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        [HttpPut]
+        public IActionResult Put(int id, Product aProduct)
+        {
+            Product existsProduct = _iProductManager.GetById(id);
+
+            if (existsProduct == null)
+                return BadRequest(new { ErrorMessage = "No product found!" });
+
+            if(ModelState.IsValid)
+            {
+                _iMapper.Map<Product, Product>(aProduct, existsProduct);
+                bool isPut = _iProductManager.Update(existsProduct);
+
+                if (isPut)
+                    return Ok();
+                else
+                    return BadRequest(new { ErrorMessage = "Product put is failed!" });
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            Product existsProduct = _iProductManager.GetById(id);
+
+            if (existsProduct == null)
+                return BadRequest();
+
+            bool isDelete = _iProductManager.Remove(existsProduct);
+
+            if (isDelete)
+                return Ok();
+            else
+                return BadRequest(new { ErrorMessage = "Product delete is failed!" });
         }
     }
 }
--- 33. Web API/HelloCoreMvcApp/HelloCoreMvcApp/Startup.cs
+++ 33. Web API/HelloCoreMvcApp/HelloCoreMvcApp/Startup.cs
@@ -49,9 +49,8 @@ namespace HelloCoreMvcApp
 
             services.AddMvc(option => option.EnableEndpointRouting = false)
                 .SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
-                .AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
-
-
+                .AddNewtonsoftJson(opt => opt.SerializerSettings
+                                             .ReferenceLoopHandling = ReferenceLoopHandling.Ignore); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
